06f57e0473b25c84591564ad5c97c607f8f222d0
postpersonality <postpersonality@gmail.com>
2019-05-15 02:20:16 +0300

    Test something meaningful

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,34 +1,54 @@
 extern crate assert_cmd;
+extern crate assert_fs;
 extern crate predicates;
 
+use std::error::Error;
 use std::process::Command;
-use assert_cmd::prelude::*;
+use std::fs::read_to_string;
 use predicates::prelude::*;
+use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
+use assert_fs::TempDir;
+
+
+fn get_cmd() -> Command {
+    Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
+}
 
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<std::error::Error>> {
-    let mut cmd = Command::new("./target/debug/serde-transcode-cli");
-    cmd.assert()
+fn file_not_specified() -> Result<(), Box<Error>> {
+    let mut cmnd = get_cmd();
+    cmnd.assert()
         .failure()
         .stderr(predicate::str::contains("No input file specified"));
 
     Ok(())
 }
 
-// use tempfile::NamedTempFile;
-// use std::io::{self, Write};
+fn test(i_fixture: &str, o_format: &str) -> Result<(), Box<Error>> {
+    let tmp_dir = TempDir::new()?;
+    let input_file = tmp_dir.child(i_fixture);
+    let input_fixture = read_to_string(format!("./tests/assets/{}", i_fixture))?;
+    input_file.write_str(&*input_fixture)?;
+    let output_file = tmp_dir.child("output");
 
-// #[test]
-// fn find_content_in_file() -> Result<(), Box<std::error::Error>> {
-//     let mut file = NamedTempFile::new()?;
-//     writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+    let mut cmnd = get_cmd();
+    cmnd.arg(input_file.path())
+        .arg("-o")
+        .arg(output_file.path())
+        .arg("-f")
+        .arg(o_format);
+    cmnd.assert()
+        .success();
 
-//     let mut cmd = Command::main_binary()?;
-//     cmd.arg("test")
-//         .arg(file.path());
-//     cmd.assert()
-//         .success()
-//         .stdout(predicate::str::contains("test\nAnother test"));
+    let output_fixture_file = format!("./tests/assets/{}.{}", i_fixture, o_format);
+    output_file.assert(predicate::path::eq_file(output_fixture_file));
 
-//     Ok(())
-// }
+    tmp_dir.close()?;
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<Error>> {
+    test("test.json", "json")
+}
